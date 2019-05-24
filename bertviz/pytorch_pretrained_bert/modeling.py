8ea1f767652e1ec49e04b1ec84cbad468d98cebe
derrywijaya <derry@seas.upenn.edu>
2019-05-24 14:43:46 -0400

    Update modeling.py

--- bertviz/pytorch_pretrained_bert/modeling.py
+++ bertviz/pytorch_pretrained_bert/modeling.py
@@ -81,7 +81,7 @@ def load_tf_weights_in_bert(model, tf_checkpoint_path):
         name = name.split('/')
         # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
         # which are not required for using pretrained model
-        if any(n in ["adam_v", "adam_m", "global_step","output_bias"] for n in name):
+        if any(n in ["adam_v", "adam_m", "global_step","output_bias","output_weights"] for n in name):
             print("Skipping {}".format("/".join(name)))
             continue
         pointer = model
@@ -95,8 +95,8 @@ def load_tf_weights_in_bert(model, tf_checkpoint_path):
             #elif l[0] == 'output_bias' or l[0] == 'beta':
             elif l[0] == 'beta':
                 pointer = getattr(pointer, 'bias')
-            elif l[0] == 'output_weights':
-                pointer = getattr(pointer, 'weight')
+            #elif l[0] == 'output_weights':
+            #    pointer = getattr(pointer, 'weight')
             elif l[0] == 'squad':
                 pointer = getattr(pointer, 'classifier')
             else:
